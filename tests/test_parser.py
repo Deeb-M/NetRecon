512db8fa2dfd72345f5ef2302e9cf8dade38c329
Deeb <deeb72@gmail.com>
2026-09-24 22:30:23 +0300

    test: require trimmed port script id

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1469,6 +1469,27 @@ class ParserTests(unittest.TestCase):
 
         self.assertIsNone(scan.hosts[0].ports[0].confidence)
 
+    def test_port_script_id_with_outer_whitespace_is_trimmed(self) -> None:
+        xml = """<nmaprun scanner="nmap">
+  <host>
+    <status state="up"/>
+    <address addr="192.0.2.10" addrtype="ipv4"/>
+    <ports>
+      <port protocol="tcp" portid="80">
+        <state state="open"/>
+        <script id=" http-title " output="Example"/>
+      </port>
+    </ports>
+  </host>
+</nmaprun>"""
+        with tempfile.TemporaryDirectory() as tmp:
+            path = Path(tmp) / "scan.xml"
+            path.write_text(xml, encoding="utf-8")
+
+            scan = parse_nmap_xml(path)
+
+        self.assertEqual(scan.hosts[0].ports[0].scripts[0].script_id, "http-title")
+
     def test_rejects_missing_file(self) -> None:
         with tempfile.TemporaryDirectory() as tmp:
             path = Path(tmp) / "missing.xml"
