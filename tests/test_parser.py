5c42bb13eba7995eb863ac69fac3063e9c9e89a8
Deeb <deeb72@gmail.com>
2026-09-24 22:21:53 +0300

    test: require trimmed service tunnel

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1259,6 +1259,27 @@ class ParserTests(unittest.TestCase):
 
         self.assertIsNone(scan.hosts[0].ports[0].extra_info)
 
+    def test_service_tunnel_with_outer_whitespace_is_trimmed(self) -> None:
+        xml = """<nmaprun scanner="nmap">
+  <host>
+    <status state="up"/>
+    <address addr="192.0.2.10" addrtype="ipv4"/>
+    <ports>
+      <port protocol="tcp" portid="443">
+        <state state="open"/>
+        <service name="http" tunnel=" ssl "/>
+      </port>
+    </ports>
+  </host>
+</nmaprun>"""
+        with tempfile.TemporaryDirectory() as tmp:
+            path = Path(tmp) / "scan.xml"
+            path.write_text(xml, encoding="utf-8")
+
+            scan = parse_nmap_xml(path)
+
+        self.assertEqual(scan.hosts[0].ports[0].tunnel, "ssl")
+
     def test_rejects_missing_file(self) -> None:
         with tempfile.TemporaryDirectory() as tmp:
             path = Path(tmp) / "missing.xml"
