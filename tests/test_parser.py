055152954372a0526a939a5984526f4a478d2fdb
Deeb <deeb72@gmail.com>
2026-09-24 22:36:04 +0300

    test: cover trimmed service CPE

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1586,6 +1586,29 @@ class ParserTests(unittest.TestCase):
 
         self.assertEqual(scan.hosts[0].scripts[0].output, "  Example output  ")
 
+    def test_service_cpe_with_outer_whitespace_is_trimmed(self) -> None:
+        xml = """<nmaprun scanner="nmap">
+  <host>
+    <status state="up"/>
+    <address addr="192.0.2.10" addrtype="ipv4"/>
+    <ports>
+      <port protocol="tcp" portid="80">
+        <state state="open"/>
+        <service name="http">
+          <cpe>  cpe:/a:example:web:1.0  </cpe>
+        </service>
+      </port>
+    </ports>
+  </host>
+</nmaprun>"""
+        with tempfile.TemporaryDirectory() as tmp:
+            path = Path(tmp) / "scan.xml"
+            path.write_text(xml, encoding="utf-8")
+
+            scan = parse_nmap_xml(path)
+
+        self.assertEqual(scan.hosts[0].ports[0].cpes, ("cpe:/a:example:web:1.0",))
+
     def test_rejects_missing_file(self) -> None:
         with tempfile.TemporaryDirectory() as tmp:
             path = Path(tmp) / "missing.xml"
