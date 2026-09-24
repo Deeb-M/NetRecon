098da54ca1896bfc8ad3293aa9526bdc5bb3894b
Deeb <deeb72@gmail.com>
2026-09-24 22:23:04 +0300

    test: cover whitespace-only service tunnel

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1280,6 +1280,27 @@ class ParserTests(unittest.TestCase):
 
         self.assertEqual(scan.hosts[0].ports[0].tunnel, "ssl")
 
+    def test_whitespace_only_service_tunnel_becomes_none(self) -> None:
+        xml = """<nmaprun scanner="nmap">
+  <host>
+    <status state="up"/>
+    <address addr="192.0.2.10" addrtype="ipv4"/>
+    <ports>
+      <port protocol="tcp" portid="443">
+        <state state="open"/>
+        <service name="http" tunnel="   "/>
+      </port>
+    </ports>
+  </host>
+</nmaprun>"""
+        with tempfile.TemporaryDirectory() as tmp:
+            path = Path(tmp) / "scan.xml"
+            path.write_text(xml, encoding="utf-8")
+
+            scan = parse_nmap_xml(path)
+
+        self.assertIsNone(scan.hosts[0].ports[0].tunnel)
+
     def test_rejects_missing_file(self) -> None:
         with tempfile.TemporaryDirectory() as tmp:
             path = Path(tmp) / "missing.xml"
