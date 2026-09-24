ff147a56c5cf5bcfa02132e1fec4aae8707f9c3e
Deeb <deeb72@gmail.com>
2026-09-24 22:36:47 +0300

    test: ignore whitespace-only service CPE

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1609,6 +1609,29 @@ class ParserTests(unittest.TestCase):
 
         self.assertEqual(scan.hosts[0].ports[0].cpes, ("cpe:/a:example:web:1.0",))
 
+    def test_whitespace_only_service_cpe_is_ignored(self) -> None:
+        xml = """<nmaprun scanner="nmap">
+  <host>
+    <status state="up"/>
+    <address addr="192.0.2.10" addrtype="ipv4"/>
+    <ports>
+      <port protocol="tcp" portid="80">
+        <state state="open"/>
+        <service name="http">
+          <cpe>   </cpe>
+        </service>
+      </port>
+    </ports>
+  </host>
+</nmaprun>"""
+        with tempfile.TemporaryDirectory() as tmp:
+            path = Path(tmp) / "scan.xml"
+            path.write_text(xml, encoding="utf-8")
+
+            scan = parse_nmap_xml(path)
+
+        self.assertEqual(scan.hosts[0].ports[0].cpes, ())
+
     def test_rejects_missing_file(self) -> None:
         with tempfile.TemporaryDirectory() as tmp:
             path = Path(tmp) / "missing.xml"
